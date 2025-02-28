60f390003db294060a985185bb78857db287aa4a
Ritikraj551 <rtkrajs551@gmail.com>
2025-03-01 00:12:19 +0530

    Added remaining funtionalites and made game functional

--- app.js
+++ app.js
@@ -1,29 +1,72 @@
-let boxes=document.querySelectorAll(".box");
-let reset=document.querySelector("#resetBtn");
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#resetBtn");
+let messagebox = document.querySelector(".msgcontainer");
+let message = document.querySelector(".msg");
 
-let turn0=true;
+let turn0 = true;
 
-const winPattern=[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+const winPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
-boxes.forEach(box=>{
-    box.addEventListener("click",()=>{
-        if(turn0==true){
-            box.innerText="X";
-            turn0=false;
-        }
-        else{
-            box.innerText="O";
-            turn0=true;
-        }
-        box.disabled=true;
-    });
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turn0 == true) {
+      box.innerText = "X";
+      turn0 = false;
+    } else {
+      box.innerText = "O";
+      turn0 = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
 });
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const resetGame = () => {
+  turn0 = true;
+  enableBoxes();
+  messagebox.classList.add("hide");
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const showWinnner = (Winner) => {
+  message.innerText = `Congratulations, Winner is ${Winner}`;
+  messagebox.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPattern) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 == pos2 && pos2 == pos3) {
+        showWinnner(pos1);
+      }
+    }
+  }
+};
+
+reset.addEventListener("click", resetGame);
